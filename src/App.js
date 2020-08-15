8c9099e2f732ad2ca89bafb432391d59fa94ecdf
cristinasofia <49313961+cristinasofia@users.noreply.github.com>
2020-08-15 14:51:12 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Binary to Decimal Converter</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+	// Definitions: React hooks
+	const [binaryText, setBinary] = useState("");
+	const [decimalText, setDecimal] = useState("");
+	// Perform the conversion on form submit
+	const onFormSubmit = (e) => {
+		e.preventDefault(); // prevents refresh of browser
+
+		// Check binary number input
+		if (binaryText.match(/^[0-1]/) == null) {
+			console.log("Error");
+			return;
+		}
+
+		setDecimal(Number.parseInt(binaryText, 2));
+	};
+	return (
+		<div className="App">
+			<h1>Binary to Decimal Converter</h1>
+			<form onSubmit={onFormSubmit}>
+				<label>
+					Binary Number:
+					<input
+						type="text"
+						value={binaryText}
+						onChange={(e) => setBinary(e.target.value)}
+					/>
+				</label>
+				<input type="submit" value="Convert" />
+				<br />
+				<label>
+					Decimal Number:
+					<input type="text" value={decimalText} disabled />
+				</label>
+			</form>
+		</div>
+	);
 }
