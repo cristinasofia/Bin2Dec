bc357e7972c136494b53ede8981a6de82f1d585f
cristinasofia <49313961+cristinasofia@users.noreply.github.com>
2020-08-15 16:11:26 -0400

    commit

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 <h1> Binary to Decimal Converter </h1>
-<p> Bin2Dec allows user to enter a string of binary digit and returns its decimal. </p>
+<p> Binary is the number system all digital computers are based on. Therefore it's important for developers to understand binary, or base 2, mathematics. The purpose of Bin2Dec is to provide practice and understanding of how binary calculations. </p>
+<p> Bin2Dec allows the user to enter strings of up to 8 binary digits, 0's and 1's, in any sequence and then displays its decimal equivalent. </p>
 <h2> Use Cases </h2>
 <li> User can enter up to 8 binary digits in one input field </li>
 <li> User must be notified if anything other than a 0 or 1 was entered </li>
@@ -7,3 +8,5 @@
 <h2> Constraints </h2>
 <li> Arrays may not be used contain the binary digits entered by the user </li>
 <li> Determining the decimal equivalent of a particular binary digit in the sequence must be calculated using a single mathematical function, for example the natural logarithm </li>
+<h2> Design </h2>
+<li> React.js </li>
--- src/App.js
+++ src/App.js
@@ -5,35 +5,42 @@ export default function App() {
   // Definitions: React hooks
   const [binaryText, setBinary] = useState("");
   const [decimalText, setDecimal] = useState("");
+  const [show, setShow] = useState(false);
   // Perform the conversion on form submit
   const onFormSubmit = (e) => {
     e.preventDefault(); // prevents refresh of browser
 
     // Check binary number input
     if (binaryText.match(/^[0-1]/) == null) {
-      console.log("Error");
+      setShow(true);
+      setDecimal("");
       return;
     }
+
     setDecimal(Number.parseInt(binaryText, 2));
   };
   return (
     <div className="App">
       <h1>Binary to Decimal Converter</h1>
       <form onSubmit={onFormSubmit}>
-        <label>
-          Binary Number:
-          <input
-            type="text"
-            value={binaryText}
-            onChange={(e) => setBinary(e.target.value)}
-          />
-        </label>
-        <input type="submit" value="Convert" />
+        <label>Binary</label>
+        <input
+          type="text"
+          value={binaryText}
+          onChange={(e) => setBinary(e.target.value)}
+        />
+        <input type="submit" value="Convert" onClick={(e) => setShow()} />
         <br />
-        <label>
-          Decimal Number:
-          <input type="text" value={decimalText} disabled />
-        </label>
+        {show && (
+          <span style={{ color: "red" }}>
+            Please only enter 0's and 1's
+            <br />
+          </span>
+        )}
+        <br />
+
+        <label>Decimal</label>
+        <input type="text" value={decimalText} disabled />
       </form>
     </div>
   );
